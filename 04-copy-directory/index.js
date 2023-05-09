38de57a7371f49f7d9bb7384fd664dcd90a38f80
Rimfis <dimitri.rim@yandex.ru>
2023-05-09 12:43:59 +0300

    fix: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,14 +1,19 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
-fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
-.then(data => {
-    data.forEach(elem => {
-        let from = path.join(__dirname, 'files', elem.name);
-        let to =  path.join(__dirname, 'files-copy', elem.name);
-        fs.copyFile( from, to, fs.constants.COPYFILE_FICLONE, err => {
-            console.log(err);
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true })
+.then(() => {
+    fs.readdir(path.join(__dirname, 'files-copy'))
+    .then(data => {
+        data.forEach(item => {
+            fs.unlink(path.join(__dirname, 'files-copy', item));
         })
     })
+    fs.readdir(path.join(__dirname, 'files'))
+    .then(data => {
+        data.forEach(item => {
+            fs.copyFile(path.join(__dirname, 'files', item), path.join(__dirname, 'files-copy', item))
+        })
+    })
+    
 })
